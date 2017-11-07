6f2db73f38668aac2a7a56ccac32a58de72a5649
watchsea <Iop[]\890>
2017-11-07 10:09:15 +0800

    change the image size to parameter

--- utils.py
+++ utils.py
@@ -4,36 +4,21 @@ import numpy as np
 import tensorflow as tf
 
 from config import cfg
+import rb_data
 
 
 def load_mnist(path, is_training):
-    fd = open(os.path.join(cfg.dataset, 'train-images-idx3-ubyte'))
-    loaded = np.fromfile(file=fd, dtype=np.uint8)
-    trX = loaded[16:].reshape((60000, 28, 28, 1)).astype(np.float)
-
-    fd = open(os.path.join(cfg.dataset, 'train-labels-idx1-ubyte'))
-    loaded = np.fromfile(file=fd, dtype=np.uint8)
-    trY = loaded[8:].reshape((60000)).astype(np.int32)
-
-    fd = open(os.path.join(cfg.dataset, 't10k-images-idx3-ubyte'))
-    loaded = np.fromfile(file=fd, dtype=np.uint8)
-    teX = loaded[16:].reshape((10000, 28, 28, 1)).astype(np.float)
-
-    fd = open(os.path.join(cfg.dataset, 't10k-labels-idx1-ubyte'))
-    loaded = np.fromfile(file=fd, dtype=np.uint8)
-    teY = loaded[8:].reshape((10000)).astype(np.int32)
-
-    # normalization and convert to a tensor [60000, 28, 28, 1]
-    trX = tf.convert_to_tensor(trX / 255., tf.float32)
-
-    # => [num_samples, 10]
-    # trY = tf.one_hot(trY, depth=10, axis=1, dtype=tf.float32)
-    # teY = tf.one_hot(teY, depth=10, axis=1, dtype=tf.float32)
-
     if is_training:
+        data = rb_data.read_data_sets("",one_hot=True)
+        trX = data.train.train_images
+        trY = data.train.train_labels
         return trX, trY
     else:
-        return teX / 255., teY
+        TEST_IMAGES = (['test_data/rb.HOT.15m(1).csv'])
+        data = rb_data.read_test_sets(TEST_IMAGES,one_hot=True)
+        teX = data.images
+        teY = data.labels
+        return teX, teY
 
 
 def get_batch_data():
